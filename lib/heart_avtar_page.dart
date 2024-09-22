224ea93fe7e4397f702f47b7b4c4980ca6f54c88
KshitijH123 <kshitijhapase@gmail.com>
2024-09-22 22:50:08 +0530

    heart avtar

---
+++ lib/heart_avtar_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:avatar_glow/avatar_glow.dart';
+
+class HeartAvtarPage extends StatelessWidget {
+  const HeartAvtarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Avatar Glow'),
+      ),
+      body: const Center(
+        child: AvatarGlow(
+          startDelay: Duration(milliseconds: 1000),
+          glowColor: Colors.blue,
+          endRadius: 200.0,
+          duration: Duration(milliseconds: 2000),
+          repeat: true,
+          child: Material(
+            elevation: 8.0,
+            shape: CircleBorder(),
+            color: Colors.transparent,
+            child: CircleAvatar(
+              backgroundImage: AssetImage("assets/images/avtar.png"),
+              radius: 75.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
