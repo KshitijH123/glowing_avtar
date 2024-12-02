58b34256010a552886a136affb6abe384cbc282f
KshitijH123 <kshitijhapase@gmail.com>
2024-12-02 16:20:14 +0530

    changes

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,6 +4,7 @@ import 'package:glowing_avtar/delivery_design.dart';
 import 'package:glowing_avtar/heart_avtar_page.dart';
 import 'package:glowing_avtar/login_page.dart';
 import 'package:glowing_avtar/phone_no_passward.dart';
+import 'package:glowing_avtar/stop_watch.dart';
 import 'avatarglow.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -85,6 +86,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 );
               },
               child: const Text('Go to phoneNumber page'),
+            ),
+             ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const StopWatch()),
+                );
+              },
+              child: const Text('Go to stopwatch page'),
             ),
           ],
         ),
